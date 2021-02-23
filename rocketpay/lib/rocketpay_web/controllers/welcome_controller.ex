77620afe59b0d6ca890211df860b9c336164c6ca
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-23 16:40:43 -0300

    create: Controller - welcome_controller.ex

--- rocketpay/lib/rocketpay_web/controllers/welcome_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/welcome_controller.ex
@@ -1,1 +1,10 @@
+defmodule RocketpayWeb.WelcomeController do
+  use RocketpayWeb, :controller
 
+  def index(connection, _params) do
+
+    text(connection, "Welcome to the Rocketpay API")
+
+  end
+
+end
