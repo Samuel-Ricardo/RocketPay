860f1477908b95054c9f97a8c7c174ae6e9a426f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-24 14:21:24 -0300

    Search Numbers file

---
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -0,0 +1,11 @@
+defmodule Rocketpay.Numbers do
+
+  def sum_from_file(filename) do
+
+      file = File.read("#{filename}.csv")
+      handle_file(file)
+  end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error,"Error: Invalid File"}
+end
