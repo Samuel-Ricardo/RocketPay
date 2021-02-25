3ef62f8a73ca5fdc0c4b7a97796d371eae33a261
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-25 17:19:53 -0300

    Create: Schema - Users

--- rocketpay/mix.exs
+++ rocketpay/mix.exs
@@ -43,9 +43,11 @@ defmodule Rocketpay.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5"}
     ]
   end
+
+  #{:credo, "~> 1.5", only: [:dev, :test], runtime: false}
 
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to install project dependencies and perform other setup tasks, run:
