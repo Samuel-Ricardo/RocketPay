bc8321c1d2056f029d65384832bec6b20b1e63d2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-25 18:51:53 -0300

    error message

--- rocketpay/lib/rocketpay_web/controllers/user_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/user_controller.ex
@@ -17,5 +17,11 @@ defmodule RocketpayWeb.UsersController do
       |> render("create.json", user: user)
   end
 
-#  defp handle_response({:error, reason}, conn) do
+  defp handle_response({:error, result}, conn) do
+
+    conn
+      |> put_status(:bad_request)
+      |> put_view(rocketpayWeb.ErrorView)
+      |> render("400.json", result: result)
+  end
 end
