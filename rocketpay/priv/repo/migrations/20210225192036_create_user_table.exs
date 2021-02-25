e31abbdeb290a4fedcac7ceb176974194cbdc9f6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-25 16:49:02 -0300

    bugfix

--- rocketpay/priv/repo/migrations/20210225192036_create_user_table.exs
+++ rocketpay/priv/repo/migrations/20210225192036_create_user_table.exs
@@ -14,8 +14,8 @@ defmodule Rocketpay.Repo.Migrations.CreateUserTable do
       timestamps()
     end
 
-    create unique_index(:user, [:email])
-    create unique_index(:user, [:nickname])
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:nickname])
 
   end
 end
