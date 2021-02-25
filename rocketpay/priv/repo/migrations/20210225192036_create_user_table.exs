2748b475eeaa3e9333ece3fe84dc32d0910f9914
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-25 16:45:32 -0300

    create: table - users

--- rocketpay/priv/repo/migrations/20210225192036_create_user_table.exs
+++ rocketpay/priv/repo/migrations/20210225192036_create_user_table.exs
@@ -3,5 +3,19 @@ defmodule Rocketpay.Repo.Migrations.CreateUserTable do
 
   def change do
 
+    create table :users do
+
+      add :name, :string
+      add :age, :integer
+      add :email, :string
+      add :password_hash, :string
+      add :nickname, :string
+
+      timestamps()
+    end
+
+    create unique_index(:user, [:email])
+    create unique_index(:user, [:nickname])
+
   end
 end
