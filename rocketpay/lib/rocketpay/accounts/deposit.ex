02b0fd10aeb97d5656c5cbd0eaa44de25c92cebd
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-28 12:09:26 -0300

    Refactor

--- rocketpay/lib/rocketpay/accounts/deposit.ex
+++ rocketpay/lib/rocketpay/accounts/deposit.ex
@@ -1,53 +1,17 @@
 defmodule Rocketpay.Accounts.Deposit do
 
   alias Ecto.Multi
-  alias Rocketpay.{Account, Repo}
+  alias Rocketpay.Accounts.Operation
+  alias Rocketpay.Repo
 
-  def call(%{"id" => id, "value" => value}) do
+  def call(params) do
 
-    Multi.new()
-      |> Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
-      |> Multi.run(:update_balance, fn repo, %{account: account} -> update_balance(repo, account, value) end)
+    params
+      |> Operation.call(:deposit)
       |> run_transaction()
   end
 
   #  params = %{"id" => "37197d60-b600-4e46-be41-64d14d5cd052", "value" => "50.0"} |> Rocketpay.deposit()
-
-  defp get_account(repo, id) do
-
-    case repo.get(Account, id) do
-
-      nil -> {:error, "Account not found!"}
-      account -> {:ok, account}
-    end
-  end
-
-  defp update_balance(repo, account, value) do
-
-    account
-      |> sum_values(value)
-      |> update_account(repo, account)
-  end
-
-  defp sum_values(%Account{balance: balance}, value) do
-
-    value
-      |> Decimal.cast()
-      |> handle_cast(balance)
-  end
-
-  defp handle_cast({:ok, value}, balance), do: Decimal.add(balance, value)
-  defp handle_cast(:error, _balance), do: {:error, "Invalid Deposit Value!"}
-
-  defp update_account({:error, _reason} = error, _repo, _account), do: error
-  defp update_account(value, repo, account) do
-
-    params = %{balance: value}
-
-    account
-      |> Account.changeset(params)
-      |> repo.update()
-  end
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
