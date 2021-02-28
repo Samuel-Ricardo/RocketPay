3eb62539c356a95597786b5d165155119f00d009
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-28 10:32:10 -0300

    run transaction

--- rocketpay/lib/rocketpay/accounts/deposit.ex
+++ rocketpay/lib/rocketpay/accounts/deposit.ex
@@ -42,4 +42,12 @@ defmodule Rocketpay.Accounts.Deposit do
       |> Account.changeset()
       |> repo.update()
   end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{update_balance: account}} -> {:ok, account}
+    end
+  end
 end
