563faa4f900ab21723eb3e3ba65276e59fb1d51a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-28 13:10:47 -0300

    transaction_view

--- rocketpay/lib/rocketpay_web/views/accounts_view.ex
+++ rocketpay/lib/rocketpay_web/views/accounts_view.ex
@@ -11,4 +11,26 @@ defmodule RocketpayWeb.AccountsView do
       }
     }
   end
+
+  def render("transaction.json", %{
+    transaction: %{to_account: to_account, from_account: from_account}
+    }) do
+
+      %{
+        message: "Transaction done Successfully",
+        transaction: %{
+          from_account: %{
+            id: from_account.id,
+            balance: from_account.balance
+          },
+
+          to_account: %{
+            id: to_account.id,
+            balance: to_account.balance
+          }
+        },
+
+      }
+
+    end
 end
