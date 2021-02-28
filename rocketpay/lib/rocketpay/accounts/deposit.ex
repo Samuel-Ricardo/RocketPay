451800bf37b2bbd0552781e46c60711b0e7fea73
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-28 10:17:26 -0300

    get account

--- rocketpay/lib/rocketpay/accounts/deposit.ex
+++ rocketpay/lib/rocketpay/accounts/deposit.ex
@@ -5,5 +5,17 @@ defmodule Rocketpay.Accounts.Deposit do
 
   def call() do
 
+    Multi.new()
+      |> Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
+      |>
+  end
+
+  defpget_account(repo, id) do
+
+    case repo.get(Account, id) do
+
+      nil -> {:erro, "Account not found! :("}
+      account -> {:ok, account}
+    end
   end
 end
