cf16afeb9976d1aac90e1f218847f2558675ed46
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-28 11:19:35 -0300

    Created: accounts_view

---
+++ rocketpay/lib/rocketpay_web/views/accounts_view.ex
@@ -0,0 +1,14 @@
+defmodule RocketpayWeb.AccountsView do
+
+  alias Rocketpay.Account
+
+  def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
+    %{
+      message: "Balance Updated Successfully"
+      account: %{
+        id: account_id,
+        balance: balance
+      }
+    }
+  end
+end
